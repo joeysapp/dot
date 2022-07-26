627bd9ae17e5af9c54cfe02839f28fe806f485c2
joeysapp <sapp.joey@gmail.com>
2022-07-26 12:59:14 -0500

    throwing basic brew install script up for now

---
+++ .dot/install.sh
@@ -0,0 +1,37 @@
+#!/bin/sh
+# make sure to chmod +x this
+echo 'Installing brew...'
+curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
+
+echo 'Installing npm...'
+brew install npm
+
+echo 'Installing python@3.10..'
+brew install python@3.10
+
+echo 'Installing figlet..'
+brew install figlet
+
+echo 'Installing cowsay..'
+brew install cowsay
+
+echo 'Installing ag/the_silver_searcher'..
+brew install ag
+
+echo 'Installing emacs cask..'
+brew install --cask emacs
+
+echo 'Installing spotify cask..'
+brew install --cask spotify
+
+echo 'Installing vlc cask..'
+brew install --cask vlc
+
+echo 'Installing discord cask..'
+brew install --cask discord
+
+echo 'Installing firefox cask..'
+brew install --cask firefox
+
+echo 'Installing tunnelblick cask..'
+brew install --cask tunnelblick

cf475155e3441a60d1fc94b8e113a900bf04cc4a
joeysapp <sapp.joey@gmail.com>
2022-07-27 20:38:49 -0500

    [emacs/init.el] transparent window, theme

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,8 +4,8 @@
 
 
 ;; visual config
-; (set-frame-parameter (selected-frame) 'alpha '(95 95))
-; (add-to-list 'default-frame-alist '(alpha 95 95))
+(set-frame-parameter (selected-frame) 'alpha '(95 95))
+(add-to-list 'default-frame-alist '(alpha 95 95))
 
 (put 'set-goal-column 'disabled nil)
 (put 'upcase-region 'disabled nil)
@@ -66,6 +66,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-enabled-themes '(sanityinc-tomorrow-eighties))
  '(custom-safe-themes
    '("58c996beb973f7e988ee4fd21c367b7a5bbdb0622ddfbbd112672a7b4e3d3b81" "a68624bd5c4bec879ee59cd3039531b6229766a8b8ed0e79eef2642f14dbda32" "afeb7b07dbc1a4cfadb24f3ef6c8cf5e63051bf76411779f03a0fe3aadc07768" "06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "bb08c73af94ee74453c90422485b29e5643b73b05e8de029a6909af6a3fb3f58" "1b8d67b43ff1723960eb5e0cba512a2c7a2ad544ddb2533a90101fd1852b426e" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" default))
  '(electric-indent-mode nil)
@@ -78,7 +79,7 @@
  '(js-jsx-align->-with-< nil)
  '(js-jsx-detect-syntax nil)
  '(package-selected-packages
-   '(moe-theme color-theme-sanityinc-tomorrow nyan-mode web-mode markdown-mode cider clojure-mode))
+   '(dumb-jump moe-theme color-theme-sanityinc-tomorrow nyan-mode web-mode markdown-mode cider clojure-mode))
  '(standard-indent 2)
  '(tool-bar-mode nil)
  '(web-mode-code-indent-offset 2)

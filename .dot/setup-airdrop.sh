01e75f4596c007d7f47cac4e5a27ac8f288f23aa
joeysapp <sapp.joey@gmail.com>
2024-03-29 19:55:25 -0500

    [airdrop] Notes for future airdrop toggling scripts

---
+++ .dot/setup-airdrop.sh
@@ -0,0 +1,7 @@
+https://apple.stackexchange.com/questions/103571/using-the-terminal-command-to-shutdown-restart-and-sleep-my-mac
+
+# Steps to (reasonably? reliably) airdrop
+1. Ensure firewall is turned off (but it can be on on foxy?)
+2. Turn wifi/bluetooth on or off
+3. Try opening up the Finder->Airdrop dialog on kittenblob
+   a. This seems to work, but still can't do copy/paste >_>
